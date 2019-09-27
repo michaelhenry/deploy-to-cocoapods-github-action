ee1309118d9ea722b076aebca3a9c56795023993
Michael Henry Pantaleon <me@iamkel.net>
2019-09-28 06:52:28 +0900

    fixed typo

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
 const shell = require('shelljs');
 shell.exec("gem install cocoapods")
-shell.exec("pod lib lint --allow-warning")
+shell.exec("pod lib lint --allow-warnings")
 shell.exec("pod trunk push --allow-warnings")
