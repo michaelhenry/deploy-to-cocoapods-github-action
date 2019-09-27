b79dcb97cdc966306e5f3b9138782a74d1044cb2
Michael Henry Pantaleon <me@iamkel.net>
2019-09-28 06:56:43 +0900

    add other info and set pipefail

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 const shell = require('shelljs');
+shell.exec("set - eo pipefail")
 shell.exec("gem install cocoapods")
+shell.exec("export LIB_VERSION = $(git describe--tags`git rev-list --tags --max-count=1`)")
 shell.exec("pod lib lint --allow-warnings")
 shell.exec("pod trunk push --allow-warnings")
