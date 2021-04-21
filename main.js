745686ab065f90596e0d5cfcf97bb2416d94262e
Michael Henry <me@iamkel.net>
2021-04-22 01:50:04 +1000

    Update main.js
    fix `set -eo pipefail`

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const shell = require('shelljs');
-shell.exec("set - eo pipefail")
+shell.exec("set -eo pipefail")
 shell.exec("gem install cocoapods")
 shell.env["LIB_VERSION"] = shell.exec("echo $(git describe --tags `git rev-list --tags --max-count=1`)")
 shell.exec("pod lib lint --allow-warnings")
